242f2836171d5848ded2431c2c3156b2317af880
josewilsoncc <josewilsoncc@gmail.com>
2015-05-05 16:18:05 -0500

    artist

--- artist.php
+++ artist.php
@@ -1,4 +1,12 @@
 <?php
 
-echo "Argumentos: ".$argc.":";
-echo var_dump($argv);
+//Total de argumentos: $argc
+//echo var_dump($argv);
+
+switch ($argv[0]){
+    case '-v':
+        $console = "DNA Version: 2015.05.05";
+        break;
+}
+
+echo $console;
