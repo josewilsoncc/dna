148ad40afd826be29df2d0e14b9c7fd529b67657
wasidev <wasidev@192.168.1.131>
2015-05-05 14:47:53 -0500

    dna update

--- startup.php
+++ startup.php
@@ -3,39 +3,7 @@
 define('DNA', BASEPATH."vendor/josewilsoncc/dna/");
 
 define('RESOURCES', BASEPATH . 'resources/');
-include DNA.'modules/orm.php';
 
-#START MUTANT
-global $blade;
-$blade = new Philo\Blade\Blade(RESOURCES . 'views', RESOURCES . '/cache');
-
-
-
-function view($view = null, $data = array(), $mergeData = array()){
-    global $blade;
-    echo $blade->view()->make($view, $data, $mergeData)->render();
-}
-
-use Illuminate\Translation\Translator;
-use Illuminate\Translation\FileLoader;
-use Illuminate\Filesystem\Filesystem;
- 
-$locale = 'es';
-$path = RESOURCES.'/lang';
-
-global $translationLoader;
-$translationLoader = new FileLoader(new Filesystem,$path);
-global $localiser;
-$localiser = new Translator($translationLoader,$locale);
-
-function trans($data){
-    global $localiser;
-    echo $localiser->get($data);
-}
-
-function set_lang($lang){
-    global $localiser;
-    global $translationLoader;
-    $localiser = new Translator($translationLoader,$lang);
-}
-#END MUTANT
+include DNA.'modules/eloquent.php';
+include DNA.'modules/blade.php';
+include DNA.'modules/i18n.php';
